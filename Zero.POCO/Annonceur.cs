8e793f88617739cc42b243baf419a7af629dc0d3
eurequat-laptop <eurequat.laptop@yopmail.com>
2013-11-06 12:16:31 +0100

    On branch iss53 	modification branch 	modified:   Zero.POCO/Annonceur.cs

--- Zero.POCO/Annonceur.cs
+++ Zero.POCO/Annonceur.cs
@@ -15,6 +15,8 @@ namespace Zero.POCO
         }
         //juste un test pour git 
         // un autre test
+
+        // la brache iss53
         public string Name {get;internal set;}
 
         protected override void Validate()
